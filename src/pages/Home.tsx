720f8626e12727c3d014cf30cc195410cbed8d19
Emmanuel-compasso <emmanuel.scortega.pb@compasso.com.br>
2023-04-29 11:33:12 -0300

    feat: Adding css to json posts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,20 +33,49 @@ const Home = () => {
           postElement.setAttribute("class", "post");
 
           const imgElement = document.createElement("img");
+          imgElement.setAttribute("class", "circular posts-img");
+
+          const postWrapper = document.createElement("div");
+          postWrapper.setAttribute("class", "posts-info");
 
           const userElement = document.createElement("p");
           const dateElement = document.createElement("p");
+
+          const postContentWrapper = document.createElement("div");
+          postContentWrapper.setAttribute("class", "posts-content")
+
           const descriptionElement = document.createElement("p");
-          const likesElement = document.createElement("p");
+          const imgContentElement = document.createElement("img");
+
+          const likesOption = document.createElement("p");
+          const commentsOption = document.createElement("p");
+          commentsOption.textContent = "Comentários";
+          const shareOption = document.createElement("p");
+          shareOption.textContent = "Compartilhar";
+
+          const optionsElement = document.createElement("div");
+          optionsElement.setAttribute("class", "post-options");
+          optionsElement.appendChild(likesOption);
+          optionsElement.appendChild(commentsOption);
+          optionsElement.appendChild(shareOption);
+
+          
+
+          const allComments = document.createElement("p");
+          allComments.setAttribute("class", "all-comments");
+          allComments.textContent = "Todos os comentários";
+
           const commentsElement = document.createElement("div");
+          commentsElement.appendChild(allComments);
 
           // Adicionar conteúdo aos elementos HTML
           imgElement.src = `https://picsum.photos/50/50?random=${num}`;
+          imgContentElement.src = `https://picsum.photos/710/300?random=${num}`;
           console.log(num);
           userElement.textContent = `${post.user}`;
           dateElement.textContent = `${post["post date"]}`;
           descriptionElement.textContent = `${post.description}`;
-          likesElement.textContent = `${post.likes}`;
+          likesOption.textContent = `Curtiu ${post.likes}`;
 
           num++;
 
@@ -55,25 +84,35 @@ const Home = () => {
           comments.forEach((comment: { user: any; comment: any }) => {
             // Criar elementos HTML para cada comentário
             const commentElement = document.createElement("div");
+            const commentImg = document.createElement("img");
+            commentImg.setAttribute("class", "circular posts-comments-img");
             const userCommentElement = document.createElement("p");
             const commentContentElement = document.createElement("p");
 
             // Adicionar conteúdo aos elementos HTML
+            commentImg.src = "https://picsum.photos/40/40";
             userCommentElement.textContent = `${comment.user}`;
             commentContentElement.textContent = `${comment.comment}`;
 
             // Adicionar elementos HTML de comentário ao elemento HTML de comentários
+            commentElement.appendChild(commentImg);
             commentElement.appendChild(userCommentElement);
             commentElement.appendChild(commentContentElement);
             commentsElement.appendChild(commentElement);
           });
 
+          // adicionar ao wrapper
+          postWrapper.appendChild(imgElement);
+          postWrapper.appendChild(userElement);
+          postWrapper.appendChild(dateElement);
+
+          postContentWrapper.appendChild(descriptionElement);
+          postContentWrapper.appendChild(imgContentElement);
+
           // Adicionar elementos HTML de post ao elemento HTML de post
-          postElement.appendChild(imgElement);
-          postElement.appendChild(userElement);
-          postElement.appendChild(dateElement);
-          postElement.appendChild(descriptionElement);
-          postElement.appendChild(likesElement);
+          postElement.appendChild(postWrapper);
+          postElement.appendChild(postContentWrapper);
+          postElement.appendChild(optionsElement);
           postElement.appendChild(commentsElement);
 
           // Adicionar elemento HTML de post à página
